fa55c733fb3c86f53c9adbd56d48ec809833a42a
iam-knr <nathrkailas@gmail.com>
2026-03-25 03:33:06 +0530

    Create public.js

---
+++ assets/src/public.js
@@ -0,0 +1,8 @@
+// Public-facing JavaScript
+// Currently no public-facing functionality needed
+// This file can be used for future enhancements
+
+jQuery(document).ready(function($) {
+    // Public-facing code here
+    console.log('BigSEO Schema Manager loaded');
+});
