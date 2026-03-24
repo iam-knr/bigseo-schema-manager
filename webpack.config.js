1aeebc83f2fc43ee9850370882673eb5caa9b58d
iam-knr <nathrkailas@gmail.com>
2026-03-25 03:14:30 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    admin: './assets/src/admin.js',
+    public: './assets/src/public.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'assets/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'jquery': 'jQuery'
+  }
+};
