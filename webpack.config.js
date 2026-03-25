cc911f0938679b6c923b1e5e6b043fded598c3aa
iam-knr <nathrkailas@gmail.com>
2026-03-26 04:41:44 +0530

    Fix webpack output path to public/dist for WordPress compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'assets/dist')
-  },
+        path: path.resolve(__dirname, 'public/dist')  },
   module: {
     rules: [
       {
